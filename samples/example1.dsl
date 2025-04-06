e4ffafea103a0b7525e117f992342c73262673ed
jacobhaileyebio <@Jacob.Yebio>
2025-04-06 14:03:28 +0200

    Initial commit

---
+++ samples/example1.dsl
@@ -0,0 +1,18 @@
+mealplan {
+    day Monday {
+        meal breakfast {
+            eggs 2
+            bacon 3
+        }
+        meal lunch {
+            salmon 200g
+            avocado 100g
+        }
+        meal dinner {
+            chicken_thigh 150g
+            spinach 50g
+        }
+    }
+}
+
+
---
+++ src/Evaluator.hs
@@ -0,0 +1,57 @@
+module Evaluator where
+
+import Data.List.NonEmpty
+
+newtype Weight = Weight Int
+  deriving (Eq, Show)
+newtype Sodium = Sodium Int
+  deriving (Eq, Show)
+newtype Potassium = Potassium Int
+  deriving (Eq, Show)
+newtype Magnesium = Magnesium Int
+  deriving (Eq, Show)
+newtype Fat = Fat Int
+
+data Food = Food
+  { foodName :: String
+  , weight :: Weight
+  , sodium :: Int
+  , potassium :: Int
+  , magnesium :: Int
+  , fat :: Int
+  }
+  deriving (Eq, Show)
+
+data Meal = Meal
+  { mealName :: String
+  , food :: [(Food, Int)]
+  }
+  deriving (Eq, Show)
+
+data DailyMealPlan = DailyMealPlan
+  { day :: CurrentDay
+  , meals :: NonEmpty Meal
+  }
+  deriving (Eq, Show)
+
+data CurrentDay = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday deriving (Eq, Show)
+
+evaluator :: [a] -> Bool
+evaluator = null
+
+-- mealPlan :: [Int] -> MealPlan
+-- day :: String -> day
+-- substitutions :: ingredient -> [Ingredient]
+-- totalCarbs :: [Int] -> Int
+-- total Sodium :: [Int] -> Int
+-- total potassium :: [Int] -> Int
+-- total magnesium :: [Int] -> Int
+-- total Fat :: [Int] -> Int
+-- maxCarbs = 20
+-- maxFat = 40
+-- minFat = 20
+-- GoalPotassium
+-- GoalSodium
+-- Goal magnesium
+
+-- allIngredients :: Meal -> [Food]
